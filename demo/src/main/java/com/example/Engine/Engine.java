4d7729b852d6e12d9c0e2b1ee38efddf21dc244f
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-06-02 13:11:53 -0400

    Engine changes

--- demo/src/main/java/com/example/Engine/Engine.java
+++ demo/src/main/java/com/example/Engine/Engine.java
@@ -29,9 +29,12 @@ public class Engine {
             
             switch (command) {
                 case "add" -> {
-                    if (inputParts.length > 1) {
+                    if (inputParts.length >= 3) {
                         String[] inp = operation.split(" ", 3);
                         String taskDescription = inp[2];
+                        if (taskDescription.startsWith("\"") && taskDescription.endsWith("\"")) {
+                                taskDescription = taskDescription.substring(1, taskDescription.length() - 1);
+                            }
                         System.out.println(taskDescription);
                         ct.addTask(taskDescription);
                     } else {
@@ -40,9 +43,18 @@ public class Engine {
                     }
 
                 case "delete" -> {
-                    int id = Integer.parseInt(inputParts[2]);
-                    String result = ct.deleteTask(id);
-                    System.out.print(result);
+                    if(inputParts.length >= 3) {
+                        try {
+                            int id = Integer.parseInt(inputParts[2]);
+                            String result = ct.deleteTask(id);
+                            System.out.print(result);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid ID. Please enter a number.");
+                        }
+                    } else {
+                        System.out.println("Please provide a TaskID for deletion.");
+                    }
+                    
                     }
 
                 case "list" -> {
@@ -55,32 +67,72 @@ public class Engine {
                 }
 
                 case "update" -> {
-                    String[] updateOperation = operation.split(" ", 4);
-                    String taskDescription = updateOperation[3];
-                        int id = Integer.parseInt(inputParts[2]);
-                        ct.updateTask(id, taskDescription);
+                    if(inputParts.length >= 4) {
+                        try {
+                            String[] updateOperation = operation.split(" ", 4);
+                            int id = Integer.parseInt(inputParts[2]);
+                            String taskDescription = updateOperation[3];
+                            if (taskDescription.startsWith("\"") && taskDescription.endsWith("\"")) {
+                                taskDescription = taskDescription.substring(1, taskDescription.length() - 1);
+                            }
+                            ct.updateTask(id, taskDescription);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid ID. Please enter a number.");
+                        }
+                    } else {
+                        System.out.println("Please provide the right TaskID and a description to update.");
+                    }
+                    
                     }
                 
                 case "mark-done" -> {
-                    int id = Integer.parseInt(inputParts[2]);
-                    ct.updateStatus("done", id);
+                    if(inputParts.length >= 3) {
+                        try {
+                            int id = Integer.parseInt(inputParts[2]);
+                            ct.updateStatus("done", id);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid ID. Please enter a number.");
+                        }
+                    } else {
+                        System.out.println("Please provide the right taskID.");
+                    }
+                    
                 }
 
                 case "mark-in-progress" -> {
-                    int id = Integer.parseInt(inputParts[2]);
-                    ct.updateStatus("in-progress", id);
+                    if(inputParts.length >= 3 ) {
+                        try {
+                            int id = Integer.parseInt(inputParts[2]);
+                            ct.updateStatus("in-progress", id);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid ID. Please enter a number.");
+                        }
+                    } else {
+                        System.out.println("Please provide the right taskID.");
+                    }
                 }
 
                 case "to-do" -> {
-                    int id = Integer.parseInt(inputParts[2]);
-                    ct.updateStatus("todo", id);
+                    if(inputParts.length >= 3 ) {
+                        try {
+                            int id = Integer.parseInt(inputParts[2]);
+                            ct.updateStatus("todo", id);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid ID. Please enter a number.");
+                        }
+                    } else {
+                        System.out.println("Please provide the right taskID.");
+                    }
                 }
 
                 default -> System.out.println("Invalid command.");
             }
+
             }
             scn.close();
         } catch (NumberFormatException e) {
+            System.out.println("An unexpected number format occured..");
+            e.printStackTrace();
         }
     }
     
